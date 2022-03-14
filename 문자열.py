ebd404947e43743b02de2805bb40d103cc25d2e5
ByeongHyeon <qudgus1984@naver.com>
2022-03-14 14:28:56 +0900

    문자열

--- 문자열.py
+++ 문자열.py
@@ -11,4 +11,10 @@ n = int(input())
 num = list(map(int, str(input())))
 print(sum(num))
 
+# 백준 알고리즘 10809
+
+n = input()
+alpha = list(range(97,123)) # 아스키코드에서 a~z까지의 범위
+for i in range(len(alpha)):
+    print(n.find(chr(alpha[i]))) 
     
