7791f7c734216b59e1f6f079e91606e67dfaae52
ByeongHyeon <qudgus1984@naver.com>
2022-03-15 15:21:07 +0900

    2292

--- 기본수학1.py
+++ 기본수학1.py
@@ -5,3 +5,16 @@ if B < C:
     print(int(A/(C-B))+1)
 else:
     print(-1)
+
+# 백준 알고리즘 2292
+
+n = int(input()) # 입력받은 숫자
+room = 1
+cnt = 1
+while n > room:
+    room += 6*cnt
+    cnt += 1
+print(cnt)
+
+            
+    
