bb452c5f50444cb4caa0fa3e6d7ba9d6477e13ed
ByeongHyeon <qudgus1984@naver.com>
2022-03-15 14:53:05 +0900

    기본수학1 1712

--- 기본수학1.py
+++ 기본수학1.py
@@ -1,8 +1,7 @@
 # 백준 알고리즘 1712
 
 A,B,C = map(int, (input().split())) # A는 고정비용, B는 가변비용, C는 노트북 가격
-X = (A + (B * X))/C
-if X == int(X):
-    print(X)
+if B < C:
+    print(int(A/(C-B))+1)
 else:
     print(-1)
